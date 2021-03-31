71d1977e15e4ce64697533fabbef1da1e9622c97
Michael Tran <mike_tran@live.com>
2021-03-30 23:03:29 -0400

    Migrate to modern Python packaging

--- python/setup.py
+++ python/setup.py
@@ -2,16 +2,4 @@
 
 from setuptools import setup
 
-setup(
-    name = 'ad_gmsa',
-    version = '0.0.1',
-    description = 'Update Kerberos keytabs based on Microsoft Active Directory group managed service accounts',
-    packages = [ 'ad_gmsa' ],
-    setup_requires = [ 'wheel' ],
-    install_requires = [ ],
-    entry_points = {
-        'console_scripts': [
-            'ad_gmsa_update_keytabs=ad_gmsa.update_keytabs:main',
-        ]
-    },
-)
+setup()
